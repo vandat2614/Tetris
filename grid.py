8271ae2007bc338e6ce4ccfc9c0420595e7f04da
Van Datt <dovandat2614@gmail.com>
2024-08-25 21:23:35 +0700

    add grid object

---
+++ grid.py
@@ -0,0 +1,18 @@
+import pygame
+
+class  Grid:
+    def __init__(self, num_rows, num_cols, cell_size, cell_colors):
+        self.num_rows = num_rows
+        self.num_cols = num_cols
+        self.cell_size = cell_size
+        self.grid = [[0] * num_cols for _ in range(num_rows)]
+        self.cell_colors = cell_colors
+        print(self.grid)
+
+    def draw(self, screen):
+        for row in range(self.num_rows):
+            for col in range(self.num_cols):
+                cell_value = self.grid[row][col]
+                cell_rect = pygame.Rect(col*self.cell_size+1, row*self.cell_size+1, 
+                                        self.cell_size-1, self.cell_size-1)
+                pygame.draw.rect(surface=screen, color=self.cell_colors[cell_value], rect=cell_rect)
--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import pygame, sys
+from grid import Grid
 
 SCREEN_WIDTH, SCREEN_HEIGHT = (300, 600)
 FPS = 60
 
 BACKGROUND_COLOR = (27, 16, 156)
+DARK_GRAY = (26, 31, 40)
+GREEN = (47, 230, 23)
+RED = (232, 18, 18)
+ORANGE = (226, 116, 17)
+YELLOW = (237, 234, 4)
+PURPLE = (166, 0, 247)
+CYAN = (21, 204, 209)
+BLUE = (13, 64, 216)
+
+CELL_COLORS = [DARK_GRAY, GREEN, RED, ORANGE, YELLOW, PURPLE, CYAN, BLUE]
 
 
 screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -11,6 +22,13 @@ pygame.display.set_caption('Tetris')
 
 clock = pygame.time.Clock()
 
+NUM_ROWS, NUM_COLS = 20, 10
+CELL_SIZE = SCREEN_WIDTH // NUM_COLS
+
+game_grid = Grid(NUM_ROWS, NUM_COLS, CELL_SIZE, CELL_COLORS)
+
+
+# event handling -> updating positions -> drawing objects
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -18,6 +36,8 @@ while True:
             sys.exit()
 
     screen.fill(BACKGROUND_COLOR)   
+    game_grid.draw(screen)
+
     pygame.display.update()
     clock.tick(FPS)
 
